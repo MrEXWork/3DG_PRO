8189f3ad4c2fa6036841f16ac93cc5a1f5a09c2e
linlang <416218050@qq.com>
2014-04-01 23:58:32 +0800

    myaccount

---
+++ PRJ_3DG/Controllers/MyAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyAccountViewController.h
+//  PRJ_3DG
+//
+//  Created by linxg on 14-4-1.
+//  Copyright (c) 2014年 Mr. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MyAccountViewController : BaseViewController
+
+@end
---
+++ PRJ_3DG/Controllers/MyAccountViewController.m
@@ -0,0 +1,38 @@
+//
+//  MyAccountViewController.m
+//  PRJ_3DG
+//
+//  Created by linxg on 14-4-1.
+//  Copyright (c) 2014年 Mr. All rights reserved.
+//
+
+#import "MyAccountViewController.h"
+
+@interface MyAccountViewController ()
+
+@end
+
+@implementation MyAccountViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

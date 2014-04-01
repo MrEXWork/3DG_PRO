fd768030cca280d427cfc70e3fd14f6f50e92ef8
xxx <xxx@xxx.com>
2014-04-01 23:54:59 +0800

    ShoppingCart Add

---
+++ PRJ_3DG/Controllers/ShoppingCartViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShoppingCartViewController.h
+//  PRJ_3DG
+//
+//  Created by MacPro-Mr.Lu on 14-4-1.
+//  Copyright (c) 2014年 Mr. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ShoppingCartViewController : BaseViewController
+
+@end

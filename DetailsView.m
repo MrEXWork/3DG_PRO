efce8da5040d1811e2854ce3e833c676104cff13
linlang <416218050@qq.com>
2014-04-09 19:46:27 +0800

    商品详情

---
+++ DetailsView.m
@@ -0,0 +1,31 @@
+//
+//  DetailsView.m
+//  PRJ_3DG
+//
+//  Created by linxg on 14-4-9.
+//  Copyright (c) 2014年 Mr. All rights reserved.
+//
+
+#import "DetailsView.h"
+
+@implementation DetailsView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
